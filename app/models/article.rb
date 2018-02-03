a9c6a76d642ce93f48af1b6562cd43d755a363ee
artem <clifredd@gmail.com>
2018-02-03 21:32:25 +0500

    Comment model, migration and reference to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
+  
   validates :title, :text, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180202094306) do
+ActiveRecord::Schema.define(version: 20180203162202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180202094306) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
   create_table "contacts", force: :cascade do |t|
     t.string "email"
     t.text "message"
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 20180202094306) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "articles"
 end
