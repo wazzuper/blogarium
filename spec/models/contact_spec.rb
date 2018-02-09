6dae636542b80fa67e3f51d2ae6ff812b56eff42
artem <clifredd@gmail.com>
2018-02-09 14:39:55 +0500

    Changed tests in contact model, added tests of comment model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
+describe Contact do
   it { should validate_presence_of :email }
   it { should validate_presence_of :message }
 end
