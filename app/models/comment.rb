3316c9c4b30fd0214255e41eb3ff95a615192fe5
artem <clifredd@gmail.com>
2018-02-09 19:03:32 +0500

    Added validations and tests for Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  validates :body, length: { maximum: 1500 }, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe 'associations' do
+    it { should belong_to :article }
+  end
+
+  describe 'validations' do
+    it { should validate_length_of(:body).is_at_most(1500) }
+  end
 end
