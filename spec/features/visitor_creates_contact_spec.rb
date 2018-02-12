68aeb806786520834dd60e5090014be25f9f7e9b
artem <clifredd@gmail.com>
2018-02-12 13:16:49 +0500

    Updated contacts test: allows a guest to fill in a form

--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -7,4 +7,14 @@ feature 'Contact Creation' do
 
     expect(page).to have_content(I18n.t 'contacts.contact_us')
   end
+
+  scenario 'allows a guest to fill in a contacts form' do
+    visit(new_contacts_path)
+
+    fill_in(:contact_email, with: 'mail@mail.com')
+    fill_in(:contact_message, with: 'message body')
+    click_button('Send message')
+
+    expect(page).to have_content('Thanks!')
+  end
 end
