6b6072aa80b208a267c41f0314ed7af5f76b68ba
artem <clifredd@gmail.com>
2018-02-12 12:54:49 +0500

    Updated contacts test

--- spec/features/visitor_creates_contact_spec.rb
+++ spec/features/visitor_creates_contact_spec.rb
@@ -5,6 +5,6 @@ feature 'Contact Creation' do
   scenario 'allows a guest to visit a contacts page' do
     visit(new_contacts_path)
 
-    expect(page).to have_content('Contact us')
+    expect(page).to have_content(I18n.t 'contacts.contact_us')
   end
 end
